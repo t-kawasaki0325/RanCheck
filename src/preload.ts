d8df9e0a946b8c22ab26f6bd59e84f9d20647beb
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2020-10-25 09:59:28 +0900

    feat: 一旦package化できるようにする

--- src/preload.ts
+++ src/preload.ts
@@ -4,6 +4,10 @@ global.cheerio = require('cheerio')
 global.datastore = require('nedb')
 // @ts-ignore
 global.recharts = require('recharts')
+// @ts-ignore
+global.path = require('path')
+// @ts-ignore
+global.pathToDb = process.env.NODE_ENV ? path.join(__dirname, '../') : __dirname
 
 const _setImmediate = setImmediate
 process.once('loaded', () => {
